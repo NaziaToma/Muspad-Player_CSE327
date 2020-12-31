6ee7b9bc1f6b57a42b058be9514ea91f7fbc37e0
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-31 18:38:09 +0600

    first frame modified

--- Code/music.py
+++ Code/music.py
@@ -10,19 +10,16 @@ class Player(tk.Frame):
 		super().__init__(master)
 		self.master = master
 		self.pack()
-		mixer.init()
-
-			self.playlist=[]
+		
+		self.playlist=[]
 
 		self.create_frames()
     
-    def add_song():
-
+    
+        
 
 	def create_frames(self):
-		self.track = tk.LabelFrame(self, text='Song Track', 
-					font=("times new roman",15,"bold"),
-					bg="grey",fg="white",bd=5,relief=tk.GROOVE)
+		self.track = tk.LabelFrame(self, font=("times new roman",15,"bold"), bg="grey",fg="white",bd=5,relief=tk.GROOVE)
 		self.track.config(width=600,height=300)
 		self.track.grid(row=0, column=0, padx=0)
 
