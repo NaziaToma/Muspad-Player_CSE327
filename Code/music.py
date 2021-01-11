8d918e9e5201456967196f9f91d414a4ebfb1682
NaziaToma <nazia.tabassum@northsouth.edu>
2021-01-11 22:39:28 +0600

    menubar added to the app

--- Code/music.py
+++ Code/music.py
@@ -3,6 +3,7 @@ import pickle
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import PhotoImage
+from tkinter import Menu
 from pygame import mixer
 
 class Player(tk.Frame):
@@ -14,14 +15,35 @@ class Player(tk.Frame):
 		self.playlist=[]
 
 		self.create_frames()
-    
-    
-        
+		self.initUI()
 
+	def initUI(self):
+
+		menubar = Menu(self.master)
+		self.master.config(menu=menubar)
+
+		fileMenu = Menu(menubar)
+		fileMenu.add_command(label="Exit", command=self.onExit)
+		menubar.add_cascade(label="Add Song", menu=fileMenu)
+
+	def onExit(self):
+
+		self.quit()
+			
 	def create_frames(self):
-		self.track = tk.LabelFrame(self, font=("times new roman",15,"bold"), bg="grey",fg="white",bd=5,relief=tk.GROOVE)
-		self.track.config(width=600,height=300)
-		self.track.grid(row=0, column=0, padx=0)
+		self.track = tk.LabelFrame(self,
+					font=("times new roman",15,"bold"),
+					bg="white",fg="white",bd=5,relief=tk.GROOVE)
+		self.track.config(width=600,height=400)
+		self.track.grid(row=0, column=0, padx=10, pady=30)
+
+		self.controls = tk.LabelFrame(self,
+							font=("times new roman",15,"bold"),
+							bg="white",fg="white",bd=2,relief=tk.GROOVE)
+		self.controls.config(width=600,height=80)
+		self.controls.grid(row=2, column=0, pady=35, padx=10)
+
+
 
 		
 root = tk.Tk()
