d79b415142dbfe4bddf8c6fbc77f3854ae185d3b
NaziaToma <nazia.tabassum@northsouth.edu>
2021-01-12 16:04:32 +0600

    modified muspad player

--- Code/music.py
+++ Code/music.py
@@ -6,16 +6,31 @@ from tkinter import PhotoImage
 from tkinter import Menu
 from pygame import mixer
 
+
 class Player(tk.Frame):
 	def __init__(self, master=None):
 		super().__init__(master)
 		self.master = master
 		self.pack()
-		
-		self.playlist=[]
+		mixer.init()
+
+		if os.path.exists('songs.pickle'):
+			with open('songs.pickle', 'rb') as f:
+				self.playlist = pickle.load(f)
+		else:
+			self.playlist=[]
+
+		self.current = 0
+		self.paused = True
+		self.played = False
 
 		self.create_frames()
+		self.track_widgets()
+		self.control_widgets()
+		self.tracklist_widgets()
+		self.volume_widgets()
 		self.initUI()
+		
 
 	def initUI(self):
 
@@ -23,32 +38,164 @@ class Player(tk.Frame):
 		self.master.config(menu=menubar)
 
 		fileMenu = Menu(menubar)
-		fileMenu.add_command(label="Exit", command=self.onExit)
+		fileMenu.add_command(label="Load Song", command=self.retrieve_songs)
 		menubar.add_cascade(label="Add Song", menu=fileMenu)
 
-	def onExit(self):
-
-		self.quit()
-			
 	def create_frames(self):
 		self.track = tk.LabelFrame(self,
-					font=("times new roman",15,"bold"),
-					bg="white",fg="white",bd=5,relief=tk.GROOVE)
-		self.track.config(width=600,height=400)
+					bg="white",bd=50,relief=tk.GROOVE)
 		self.track.grid(row=0, column=0, padx=10, pady=30)
 
-		self.controls = tk.LabelFrame(self,
+		self.tracklist = tk.LabelFrame(self, text=f'PlayList - {str(len(self.playlist))}',
 							font=("times new roman",15,"bold"),
-							bg="white",fg="white",bd=2,relief=tk.GROOVE)
-		self.controls.config(width=600,height=80)
-		self.controls.grid(row=2, column=0, pady=35, padx=10)
+							bg="white",fg="black",relief=tk.FLAT)
+		self.tracklist.grid(row=0, column=1)
+
+		self.controls = tk.LabelFrame(self,
+							bg="white",fg="white",bd=2,relief=tk.RAISED)
+		self.controls.grid(row=2, column=0, pady=5, padx=10)
+
+		self.volumecontrol = tk.LabelFrame(self,
+							bg="white",fg="black",relief=tk.FLAT)
+		self.volumecontrol.config(width=130,height=50)
+		self.volumecontrol.grid(row=2, column=1)
+
+
+	def track_widgets(self):
+		self.canvas = tk.Label(self.track, image=img)
+		self.canvas.configure(width=550, height=300)
+		self.canvas.grid(row=0,column=0)
+
+		self.songtrack = tk.Label(self.track, font=("times new roman",16,"bold"),
+						bg="white",fg="Blue")
+		self.songtrack.config(width=30, height=1)
+		self.songtrack.grid(row=1,column=0,padx=10)
+
+	def control_widgets(self):
+		self.prev = tk.Button(self.controls, image=prev)
+		self.prev.configure(width=110, height=80)
+		self.prev['command'] = self.prev_song
+		self.prev.grid(row=0, column=1)
+
+		self.pause = tk.Button(self.controls, image=pause)
+		self.pause.configure(width=110, height=80)
+		self.pause['command'] = self.pause_song
+		self.pause.grid(row=0, column=2)
+
+		self.next = tk.Button(self.controls, image=next_)
+		self.next.configure(width=110, height=80)
+		self.next['command'] = self.next_song
+		self.next.grid(row=0, column=3)
 
 
 
-		
+	def tracklist_widgets(self):
+		self.scrollbar = tk.Scrollbar(self.tracklist, orient=tk.VERTICAL)
+		self.scrollbar.grid(row=0,column=1, rowspan=5, sticky='ns')
+
+		self.list = tk.Listbox(self.tracklist, selectmode=tk.SINGLE,
+					 yscrollcommand=self.scrollbar.set, selectbackground='grey')
+		self.enumerate_songs()
+		self.list.config(height=22)
+		self.list.bind('<Double-1>', self.play_song) 
+
+		self.scrollbar.config(command=self.list.yview)
+		self.list.grid(row=0, column=0, rowspan=5)
+
+
+	def volume_widgets(self):
+
+		self.volume = tk.DoubleVar(self)
+		self.slider = tk.Scale(self.volumecontrol, from_ = 0, to = 10, orient = tk.HORIZONTAL)
+		self.slider['variable'] = self.volume
+		self.slider.set(7)
+		mixer.music.set_volume(0.8)
+		self.slider['command'] = self.change_volume
+		self.slider.grid(row=0, column=4, padx=5)
+
+	def retrieve_songs(self):
+		self.songlist = []
+		directory = filedialog.askdirectory()
+		for root_, dirs, files in os.walk(directory):
+				for file in files:
+					if os.path.splitext(file)[1] == '.mp3':
+						path = (root_ + '/' + file).replace('\\','/')
+						self.songlist.append(path)
+
+		with open('songs.pickle', 'wb') as f:
+			pickle.dump(self.songlist, f)
+		self.playlist = self.songlist
+		self.tracklist['text'] = f'PlayList - {str(len(self.playlist))}'
+		self.list.delete(0, tk.END)
+		self.enumerate_songs()
+
+	def enumerate_songs(self):
+		for index, song in enumerate(self.playlist):
+			self.list.insert(index, os.path.basename(song))
+
+
+	def play_song(self, event=None):
+		if event is not None:
+			self.current = self.list.curselection()[0]
+			for i in range(len(self.playlist)):
+				self.list.itemconfigure(i, bg="white")
+
+		print(self.playlist[self.current])
+		mixer.music.load(self.playlist[self.current])
+		self.songtrack['anchor'] = 'w' 
+		self.songtrack['text'] = os.path.basename(self.playlist[self.current])
+
+		self.pause['image'] = play
+		self.paused = False
+		self.played = True
+		self.list.activate(self.current) 
+		self.list.itemconfigure(self.current, bg='sky blue')
+
+		mixer.music.play()
+
+	def pause_song(self):
+		if not self.paused:
+			self.paused = True
+			mixer.music.pause()
+			self.pause['image'] = pause
+		else:
+			if self.played == False:
+				self.play_song()
+			self.paused = False
+			mixer.music.unpause()
+			self.pause['image'] = play
+
+	def prev_song(self):
+		if self.current > 0:
+			self.current -= 1
+		else:
+			self.current = 0
+		self.list.itemconfigure(self.current + 1, bg='white')
+		self.play_song()
+
+	def next_song(self):
+		if self.current < len(self.playlist) - 1:
+			self.current += 1
+		else:
+			self.current = 0
+		self.list.itemconfigure(self.current - 1, bg='white')
+		self.play_song()
+
+	def change_volume(self, event=None):
+		self.v = self.volume.get()
+		mixer.music.set_volume(self.v / 10)
+
+# ----------------------------- Main -------------------------------------------
+
 root = tk.Tk()
-root.geometry('800x600')
-root.wm_title('MusPad Player')
+root.geometry('900x600')
+root.wm_title('Muspad Player')
+
+img = PhotoImage(file='images/bg.gif')
+next_ = PhotoImage(file = 'images/next.gif')
+prev = PhotoImage(file='images/previous.gif')
+play = PhotoImage(file='images/play.gif')
+pause = PhotoImage(file='images/pause.gif')
 
 app = Player(master=root)
 app.mainloop()
